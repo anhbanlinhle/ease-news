ad0326fb6c0b90d16c0ef16ce354a4b97e789802
htq-uet <teoemga2003@gmail.com>
2024-11-17 23:52:07 +0700

    update data and category

--- backend/src/controller/news/getAllCategories.js
+++ backend/src/controller/news/getAllCategories.js
@@ -3,6 +3,8 @@ import news from '../../../data/mockNews.json'
 let allCategories = async (req, res) => {
   try {
     let categories = news.flatMap((item) => item.categories)
+    let excludeCategories = ['Xu Hướng', 'Gợi Ý']
+    categories = categories.filter((item) => !excludeCategories.includes(item))
     let uniqueCategories = [...new Set(categories)]
     return res.status(200).send(uniqueCategories.sort())
   }
