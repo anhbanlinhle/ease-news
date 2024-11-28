285c8e455c0ebb08df1391befe05e1ff2f4a13c3
anhbanlinhle <vietlinhleviet@gmail.com>
2024-11-28 08:23:15 +0700

    feat[lynn]: replace slice with rtk actions

--- mobile/redux/reducers/newsSlice.js
+++ mobile/redux/reducers/newsSlice.js
@@ -1,53 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit"
+import {createAction} from "@reduxjs/toolkit"
 
-export const newsSlice = createSlice({
-    name: 'newsData',
-    initialState: {
-        news: [],
-        categories: [],
-        loading: false,
-        error: '',
-    },
-    reducers: {
-        getNewsAction: (state) => {
-            state.loading = true
-            state.error = ''
-        },
-        getNewsSuccessAction: (state, action) => {
-            state.loading = false
-            state.news = action.payload
-        },
-        getAllCategoriesAction: (state) => {
-            state.loading = true
-            state.error = ''
-        },
-        getCategorySuccessAction: (state, action) => {
-            state.loading = false
-            state.categories = action.payload
-        },
-        getNewsByCategoryAction: (state) => {
-            state.loading = true
-            state.error = ''
-        },
-        getNewsFailureAction: (state, action) => {
-            state.loading = false
-            state.error = action.payload
-        },
-    }
-})
+export const getNewsAction = createAction(
+  'newsData/getNewsAction'
+)
 
-export const {
-    getNewsAction,
-    getNewsSuccessAction,
-    getAllCategoriesAction,
-    getCategorySuccessAction,
-    getNewsByCategoryAction,
-    getNewsFailureAction,
+export const getAllCategoriesAction = createAction(
+  'newsData/getAllCategoriesAction'
+)
 
-} = newsSlice.actions
-
-export const selectNews = (state) => state.newsData.news
-export const selectLoading = (state) => state.newsData.loading
-export const selectError = (state) => state.newsData.error
-
-export default newsSlice.reducer
+export const getNewsByCategoryAction = createAction(
+  'newsData/getNewsByCategoryAction'
+)
