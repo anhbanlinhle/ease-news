c0025fc6ae5566cf07d1cef2b0d5718b92589c34
anhbanlinhle <vietlinhleviet@gmail.com>
2024-12-11 02:01:13 +0700

    feat: change cuda to cpu

--- service/app/utils/viToEnTranslator.py
+++ service/app/utils/viToEnTranslator.py
@@ -9,14 +9,14 @@ def initialize_translator():
     translate_model_name = "VietAI/envit5-translation"
     translate_tokenizer = AutoTokenizer.from_pretrained(translate_model_name)
     translate_model = AutoModelForSeq2SeqLM.from_pretrained(translate_model_name)
-    translate_model.to('cuda')
+    translate_model.to('cpu')
 
 
 def translate_text(sentence):
     if translate_model is None or translate_tokenizer is None:
         initialize_translator()
     output = translate_model.generate(
-        translate_tokenizer(sentence, return_tensors="pt", padding=True).input_ids.to('cuda'), max_length=512)
+        translate_tokenizer(sentence, return_tensors="pt", padding=True).input_ids.to('cpu'), max_length=512)
     sentence = translate_tokenizer.batch_decode(output, skip_special_tokens=True)
     en_sentence = sentence[0].split("en: ")[1]
     return en_sentence
